afc9d7d06e4e7b2fb0403f44ab0bcabfca9d5365
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-02-28 05:53:25 -0600

    add flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Kiri's NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux"];
+
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShell = forAllSystems (
+      system: let
+        pkgs = nixpkgsFor.${system};
+      in
+        pkgs.mkShell
+        {
+          buildInputs = with pkgs; [
+            stylua
+          ];
+        }
+    );
+  };
+}
