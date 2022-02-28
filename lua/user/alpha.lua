e91d211876538ff7530a802aa76fa8c428ad6b65
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-02-28 05:51:02 -0600

    customize alpa

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Welcome back, Kiri!"
 end
 
 dashboard.section.footer.val = footer()
