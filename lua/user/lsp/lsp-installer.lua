8b2c00a5ffb52aaab57fc976045de677d0db3dd5
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-03-09 02:31:46 -0600

    use a single opts declaration for lsp

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -3,14 +3,13 @@ if not status_ok then
 	return
 end
 
+local opts = {
+	on_attach = require("user.lsp.handlers").on_attach,
+	capabilities = require("user.lsp.handlers").capabilities,
+}
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
 lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("user.lsp.handlers").on_attach,
-		capabilities = require("user.lsp.handlers").capabilities,
-	}
-
 	if server.name == "jsonls" then
 		local jsonls_opts = require("user.lsp.settings.jsonls")
 		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
@@ -26,10 +25,6 @@ lsp_installer.on_server_ready(function(server)
 	server:setup(opts)
 end)
 
-local opts = {
-	on_attach = require("user.lsp.handlers").on_attach,
-	capabilities = require("user.lsp.handlers").capabilities,
-}
 require("lspconfig").rnix.setup({ opts })
 require("lspconfig").clangd.setup(opts)
 local lua_opts = vim.tbl_deep_extend("force", require("user.lsp.settings.sumneko_lua"), opts)
