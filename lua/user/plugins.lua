098976f25e93ab0dfbe560023fb22afa2d55e72b
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-28 11:55:23 -0600

    remove unused colorscheme

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -76,7 +76,6 @@ return packer.startup(function(use)
 	})
 
 	-- Colorschemes
-	-- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
 	use("RRethy/nvim-base16")
 
 	-- cmp plugins
