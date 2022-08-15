fb37f01106f9790708462608ea6ada3708255d0a
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-15 03:03:01 -0600

    remove formatters in null-ls

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,9 +12,6 @@ null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
 		formatting.shfmt.with({ extra_args = { "--posix --simplify --write" } }),
-		-- diagnostics.flake8
 	},
 })
