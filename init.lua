f9b46c403c319d273115f7a90edf37a61dbc8a45
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-24 20:57:51 -0600

    add TODO

--- init.lua
+++ init.lua
@@ -11,7 +11,7 @@ end
 require('packer').startup(function(use)
   use 'wbthomason/packer.nvim' -- Package manager
   use 'tpope/vim-fugitive' -- Git commands in nvim
-  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github TODO: Consider removing
   use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Add git related info in the signs columns and popups
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use { 'nvim-treesitter/nvim-treesitter', run = ":TSUpdate", } -- Highlight, edit, and navigate code
