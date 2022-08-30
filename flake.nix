151518575874f64023828e58a34e3cfc7dcadee1
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-30 04:24:35 -0600

    format flake

--- flake.nix
+++ flake.nix
@@ -4,22 +4,24 @@
   inputs.devshell.url = "github:numtide/devshell";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = {
-    self,
-    flake-utils,
-    devshell,
-    nixpkgs,
-  }:
+  outputs =
+    { self
+    , flake-utils
+    , devshell
+    , nixpkgs
+    ,
+    }:
     flake-utils.lib.eachDefaultSystem (system: {
-      devShell = let
-        pkgs = import nixpkgs {
-          inherit system;
+      devShell =
+        let
+          pkgs = import nixpkgs {
+            inherit system;
 
-          overlays = [devshell.overlay];
-        };
-      in
+            overlays = [ devshell.overlay ];
+          };
+        in
         pkgs.devshell.mkShell {
-          imports = [(pkgs.devshell.importTOML ./devshell.toml)];
+          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
         };
     });
 }
