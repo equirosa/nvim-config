c0b9b59003e42a3c2a29ec2d6a470e21db80abb9
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-08-31 11:37:11 -0600

    fix strings

--- init.lua
+++ init.lua
@@ -380,14 +380,14 @@ for _, lsp in ipairs(nix_servers) do
   })
 end
 
-vim.o.foldcolumn = '1'
+vim.o.foldcolumn = "1"
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-require('ufo').setup()
+require("ufo").setup()
 
-vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
 
 -- Example custom configuration for lua
 --
